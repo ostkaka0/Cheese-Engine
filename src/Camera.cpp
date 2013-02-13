699b1ee3e14b6c8251f484599c799e15dc917cae
john <john.ostkaka@gmail.com>
2013-02-13 14:03:47 +0100

    ingen updatering.

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,4 +1,7 @@
 #include "Camera.h"
+#include <math.h>
+
+#define PI 3.14159265358979323846264338327950288419
 
 Camera::Camera(double speed)
 {
@@ -16,8 +19,8 @@ void Camera::Update(sf::RenderWindow &app)
 {
 	if (currentCreature != NULL)
 		this->SetCenter(
-		(currentCreature->getX()+8)*speed*app.GetFrameTime()+
-		this->GetCenter().x*(1-speed*app.GetFrameTime()),
+		(currentCreature->getX()+8)*speed/atan(app.GetFrameTime()*PI)+
+		this->GetCenter().x*(1-speed/atan(app.GetFrameTime()*PI)),
 		(currentCreature->getY()+8)*speed*app.GetFrameTime()+
 		this->GetCenter().y*(1-speed*app.GetFrameTime()));
 }
