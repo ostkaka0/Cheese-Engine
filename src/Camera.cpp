e1658704529c58b9c1025bebb8981162f96af5ac
john <john.ostkaka@gmail.com>
2013-03-28 17:45:55 +0100

    camera fix!

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -15,11 +15,13 @@ Camera::~Camera(void)
 void Camera::Update(sf::RenderWindow &app)
 {
 	if (currentCreature != NULL)
-		this->SetCenter(
-		(currentCreature->getX()+8)*speed*app.GetFrameTime()+
-		this->GetCenter().x*(1-speed*app.GetFrameTime()),
-		(currentCreature->getY()+8)*speed*app.GetFrameTime()+
-		this->GetCenter().y*(1-speed*app.GetFrameTime()));
+	{
+		float deltaX = currentCreature->getX()+8-GetCenter().x;
+		float deltaY = currentCreature->getY()+8-GetCenter().y;
+		float speedFactor = atan(app.GetFrameTime()*speed)*2/3.14159265358979323846264338327950288419;
+
+		SetCenter(GetCenter().x + deltaX*speedFactor, GetCenter().y + deltaY*speedFactor);
+	}
 }
 
 void Camera::setCameraAt(Creature &creature)
