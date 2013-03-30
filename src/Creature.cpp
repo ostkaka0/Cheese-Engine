682205727263f0057e444896294fd31088d00ea9
Gustav Hammar <gustav9797@hotmail.se>
2013-03-30 12:17:49 +0100

    denna...

--- src/Creature.cpp
+++ src/Creature.cpp
@@ -12,6 +12,7 @@ Creature::Creature(float x, float y, short sizeX, short sizeY, float speed, floa
 
 void Creature::Update(sf::RenderWindow &app, sf::View &camera)
 {
+	
     speedX += horizontal * app.GetFrameTime();
     speedY += vertical * app.GetFrameTime();
 
