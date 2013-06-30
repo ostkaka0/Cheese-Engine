a16cdf0d7b507e8036312fb9cc1e328d1cb681a8
john <john.ostkaka@gmail.com>
2013-06-30 17:38:44 +0200

    koskit

--- src/ServerState.cpp
+++ src/ServerState.cpp
@@ -14,7 +14,8 @@
 ServerState::ServerState(App& app)
 {
 	currentWorld = new World();
-
+	sC = new ServerConnection(5001);
+	sC->Launch();
 	//unsigned short i = 1;
 
 	//currentWorld->RegisterBlock(i,(new BlockSolid(i))->RegisterBlock(i++));
@@ -28,7 +29,7 @@ ServerState::~ServerState()
 
 GameState *ServerState::Update(App& app)
 {
-	std::cout << "updates per second: " << 1/APP(app).GetFrameTime() << std::endl;
+	//std::cout << "updates per second: " << 1/APP(app).GetFrameTime() << std::endl;
 	currentWorld->Update(app, tC);
 	return this;
 }
