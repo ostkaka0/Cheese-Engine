5203bec34c887e96df4d383214ab372307cb6f7b
Gustav Hammar <gustav9797@hotmail.se>
2013-07-05 18:42:51 +0200

    SÅXD

--- src/App.cpp
+++ src/App.cpp
@@ -1,10 +1,8 @@
-#ifdef _SERVER
+//#ifdef _SERVER
 #include "App.h"
-#include <SFML\System.hpp>
 
 #define MIN_FRAME_TIME 0.00390625F
 
-<<<<<<< HEAD
 #ifndef _SERVER
 App::App(sf::VideoMode videoMode) : RenderWindow(videoMode, "Cheese Multiplayer - Alpha")
 {
@@ -13,32 +11,29 @@ App::App(sf::VideoMode videoMode) : RenderWindow(videoMode, "Cheese Multiplayer 
 #endif
 
 float App::getFrameTime()
-=======
-float App::GetFrameTime()
->>>>>>> 3e2d1d23fd7a70df773ec0d22345bb1ca4eedc82
 {
 	return frameTime+sleptTime;
 }
 
 void App::Update()
 {
-	frameTime = frameTimer.GetElapsedTime();
+	frameTime = frameTimer.getElapsedTime().asMilliseconds();
 
 	if (frameTime < MIN_FRAME_TIME)
 	{
 		sleptTime = MIN_FRAME_TIME-frameTime;
-		sf::Sleep(sleptTime);
+		sf::sleep(sf::milliseconds(sleptTime));
 	}
 	else
 	{
 		sleptTime = 0;
 	}
 
-	frameTimer.Reset();
+	frameTimer.restart();
 }
 
 sf::Clock& App::getFrameTimer()
 {
 	return frameTimer;
 }
-#endif
+//#endif
